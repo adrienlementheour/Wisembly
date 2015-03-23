bfcd3c7b932864d93b9d7e8d931961e0d7904355
ElisabethStereosuper <elisabeth@Chiot.local>
2015-03-23 18:49:50 +0100

    Mise en place

---
+++ interieure.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html lang='fr' class='no-js lt-ie9'> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang='fr' class='no-js'> <!--<![endif]-->
+<html lang='fr'>
+
+	<head>
+	  	<meta charset='utf-8'>
+	  	<title>Wisembly</title>
+
+	  	<meta name='description' content=''>
+	  	<meta name='viewport' content='width=device-width,initial-scale=1'>		
+	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	  	<link rel="stylesheet" href="css/style.css">
+	  	<script src="js/modernizr-min.js" type="text/javascript"></script>
+	</head>
+
+	<body>
+		
+		<?php include('includes/header.html'); ?>
+
+		<div class='content simple' role='main'>
+			
+			<h1>Page intérieure type</h1>
+
+			<p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+				Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper 
+				nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p>
+
+			<h2>Présentation du site</h2>
+
+			<p>Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue.</p>
+
+			<h3>Aenean lacinia bibendum nulla sed consectetur.</h3>
+
+			<p>Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at eget metus. Donec id elit non mi porta gravida at 
+				eget metus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Donec 
+				sed odio dui. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. 
+				<a href=''>Lorem ipsum dolor</a> sit amet, consectetur adipiscing elit. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, 
+				nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer 
+				posuere erat a ante venenatis dapibus posuere velit aliquet. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+
+			<h3>bibendum nulla sed</h3>
+
+			<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Praesent commodo cursus magna, vel scelerisque 
+				nisl consectetur et. Etiam porta sem <strong>malesuada magna mollis</strong> euismod. Curabitur blandit tempus porttitor. Cras justo odio, 
+				dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum.<br/><br/>
+
+			Cras mattis consectetur purus sit amet fermentum. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, 
+			nascetur ridiculus mus. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus 
+			et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+			<h2>Malesuada Ligula Vehicula Consectetur</h2>
+
+			<p>Vestibulum id ligula porta felis euismod semper. Donec sed odio dui. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, 
+				ut fermentum massa justo sit amet risus. </p>
+
+			<h2>Consectetur Ridiculus Dapibus Adipiscing</h2>
+
+			<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cum sociis natoque 
+				penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget 
+				lacinia odio sem nec elit.
+
+			Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Lorem ipsum dolor sit amet, 
+			consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
+
+			<h2>Sollicitudin Fermentum</h2>
+
+			<p>Les côtes de la Sémantique, un vaste océan de langues.</p>
+
+			<blockquote>
+				Le pilotage des questions live et évaluations permet d'aboutir facilement et rapidement à des décisions partagées, co-construites, là où 
+				la conversation téléphonique seule renvoie souvent l'animateur à sa solitude.
+			</blockquote>
+
+			<p>Il s'agit par exemple :</p>
+			<ul>
+				<li>d'un nom</li>
+				<li>d'une adresse postale</li>
+				<li>d'un numéro de téléphone</li>
+			</ul>
+
+			<p>Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor. Nullam id dolor id nibh ultricies 
+				vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, 
+				est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+
+
+		</div>
+
+	  	<?php include('includes/footer.html'); ?>
+
+	</body>
+
+</html>
