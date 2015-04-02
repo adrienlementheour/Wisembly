34239d54568a193d15f623ddc02dd24f7c6ac248
ElisabethStereosuper <elisabeth@Chiot.local>
2015-04-02 13:19:11 +0200

    Truc

--- interieure.php
+++ interieure.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<!--[if IE 8]>         <html lang='fr' class='no-js lt-ie9'> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang='fr' class='no-js'> <!--<![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
 <html lang='fr'>
 
 	<head>
