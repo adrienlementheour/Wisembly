2a1ea3cdadb743ba28cf264208e7686eaad5ea78
adrienlementheour <adrien.le.mentheour@gmail.com>
2015-05-07 18:13:33 +0200

    Page formations

---
+++ formations.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang='fr'>
+
+	<head>
+	  	<meta charset='utf-8'>
+	  	<title>Wisembly</title>
+
+	  	<meta name='description' content=''>
+
+	  	<?php include('includes/head.html'); ?>
+
+	</head>
+
+	<body class="page-template-evenements">
+
+		<div class='overflow'>
+		
+			<?php include('includes/header.html'); ?>
+			
+			<section class='head headEvenements'>
+				<div id="bgHead"></div>
+				<div class='content main'>
+					<div class='blocH1'>
+						<h1>Plus d’intéractivité <br/><span>dans vos cours<br /> et formations</span></h1>
+						<p class='subHeading'>Améliorez l'expérience des apprenants et suivez leur évolution.</p>
+					</div>
+				</div>
+			</section>
+
+			<div class='wrapper' role='main'>
+				
+				<div id="bg-blanc"></div>
+				
+				<section class='content bgBlanc scrollHere'>
+					<h2 class="num noMarginTop">Concentrez vos cours</h2>
+					<h3>Partagez des documents avec vos apprenants avant la formation.</h3>
+					<p>Dynamisez vos formations : <strong>centralisez tous les supports au même endroit,</strong> impliquez vos apprenants et boostez leur engagement.</p>
+					<div id="slider-visez-juste" class="slider-evenements carousel">
+						<ul>
+							<li>
+								<img src="img/evenements/illus-slider-visez-juste1.png">
+							</li>
+							<li>
+								<img src="img/evenements/illus-slider-visez-juste2.png">
+							</li>
+							<li>
+								<img src="img/evenements/illus-slider-visez-juste3.png">
+							</li>
+						</ul>
+					</div>
+					<blockquote class='blue'><span class="raccord-img"></span>Au-delà de l’évidente dynamique ludique que crée Wisembly, il redonne à nos pratiques d’intervention un gain spectaculaire dans la capacité d’interaction.</blockquote>
+					<div class="citeContainer">
+						<span class="imgQuote"><img src="img/evenements/olivier-lagree.jpg"></span>
+						<span class="cite">
+							<strong>Georges-Edouard Dias</strong>
+							Professeur à HEC
+						</span>
+					</div>
+					<button class='icon-down scrollNext'><span></span></button>
+				</section>
+				
+				<section class='content bgBlanc scrollHere'>
+					<h2 class="num">Innovez et testez vos apprenants simplement</h2>
+					<h3>Captez votre auditoire et rythmez vos formations avec une touche digitale :</h3>
+					<p><strong>Diffusez vos slides ou vos vidéos en temps réel</strong>, et ponctuez la session de questions rapides.<br /> Vous pourrez mesurer le taux de compréhension. Gagnez du temps : proposez des évaluations pendant la session, corrigées automatiquement et dans l'instant.</p>
+					<div class="wrapper-ecran">
+						<div id="slider-engagez-audience" class="slider-evenements carousel">
+							<ul>
+								<li>
+									<img src="img/evenements/visu-engagez-audience.png">
+								</li><li>
+									<img src="img/evenements/visu-engagez-audience.png">
+								</li>
+							</ul>
+						</div>
+					</div>
+					<h3 class="marginTop">Wisembly est disponible partout, tout le temps.</h3>
+					<p>Ne changez pas les habitudes de votre public, utilisez les et surfez sur le digital : web, mobile, tablette, Twitter, email, SMS…<br />
+					Sans application à télécharger, Wisembly fonctionne depuis tous les appareils et se prête parfaitement aux conf calls multisites.</p>
+					<img class="illus-responsive" src="img/evenements/illus-responsive.png">
+					<blockquote class="blue"><span class="raccord-img"></span>Wisembly est un booster de ROI des formations et de l'enseignement. Cela devient "le manuel interactif" qui accompagne l'ensemble des participants, en communauté avec leurs formateurs.</blockquote>
+					<div class="citeContainer">
+						<span class="imgQuote"><img src="img/evenements/marie-laurence-pouxviel.jpg"></span>
+						<span class="cite">
+							<strong>Georges-Edouard Dias</strong>
+							Professeur à HEC
+						</span>
+					</div>
+					<button class='icon-down scrollNext'><span></span></button>
+				</section>
+				
+				<section class='content bgBlanc scrollHere'>
+					<h2 class="num">Gardez le contact</h2>
+					<h3>Ne laissez aucune question sans réponse</h3>
+					<p>Il n’y a pas de commentaire ou de question bête. Chaque question devrait être traitée. Avec Wisembly, vos apprenants peuvent poser leurs questions à tout moment avant, pendant ou après la formation.</p>
+					<div class="wrapper-ecran">
+						<div id="slider-contenu-evenement" class="slider-evenements carousel">
+							<ul>
+								<li>
+									<img src="img/evenements/visu-capitalisez-contenu-evenement1.png">
+								</li><li>
+									<img src="img/evenements/visu-capitalisez-contenu-evenement1.png">
+								</li>
+							</ul>
+						</div>
+					</div>
+					<h3>NCapitalisez sur tout le contenu</h3>
+					<p>Wisembly est la mémoire de vos formations. Après chaque session, vous avez accès à une <strong>analyse détaillée de votre formation</strong> : volume et type d’interactions, contenus, classement des élèves, notes moyenne etc. Exportez tous vos contenus sous format excel.</p>
+					<button class='icon-down scrollNext'><span></span></button>
+				</section>
+				
+				<section class='content bgBlanc scrollHere'>
+					<h3 class="light">TOP 3 des bénéfices de la solution</h3>
+					<ul class="listeChiffres benefices-solution">
+						<li>
+							<span class="chiffre">97%</span>
+							<span>de nos clients estiment que<span class='legend'>Wisembly améliore<br /> la participation lors des réunions</span></span>
+						</li><li>
+							<span class="chiffre">95%</span>
+							<span>de nos clients estiment que<span class='legend'>Wisembly les aide<br /> dans leur transformation digitale</span></span>
+						</li><li>
+							<span class="chiffre">90%</span>
+							<span>de nos clients estiment que<span class='legend'>Wisembly améliore<br /> la compréhension des messages</span></span>
+						</li>
+					</ul>
+					<button class='icon-down scrollNext'><span></span></button>
+				</section>
+				
+				<section class='content bgBlanc scrollHere paddingBottom' id='etudes'>
+					<h2>Ils utilisent Wisembly</h2>
+
+					<article class='rouge ref etude'>
+						<span class='img'></span>
+						<a href='#' class='cat'>Séminaires & Evénements</a>
+						<a href='#' title='' class='lien'>
+							<strong class='medium'>Convention de présentation du plan digital du groupe La Poste</strong>
+							<span class='txt'>Chaque année, La Poste organise un séminaire d'échanges de bonnes pratiques. 120 managers de la direction financière sont invités à travailler sur un thème défini par avance.</span>
+							<span class='btnFalse'>Lire la suite</span>
+							<span class='hover'>
+								<span class='quote'></span>
+								<span>
+									<span class='imgQuote'></span>
+									<span class='cite'>
+										<strong></strong>
+									</span>
+								</span>
+							</span>
+						</a>
+					</article><article class='jaune ref etude'>
+						<span class='img'></span>
+						<a href='#' class='cat'>Séminaires & Evénements</a>
+						<a href='#' title='' class='lien'>
+							<strong class='medium'>Convention de présentation du plan digital du groupe La Poste</strong>
+							<span class='txt'>Chaque année, La Poste organise un séminaire d'échanges de bonnes pratiques. 120 managers de la direction financière sont invités à travailler sur un thème défini par avance.</span>
+							<span class='btnFalse'>Lire la suite</span>
+							<span class='hover'>
+								<span class='quote'></span>
+								<span>
+									<span class='imgQuote'></span>
+									<span class='cite'>
+										<strong></strong>
+									</span>
+								</span>
+							</span>
+						</a>
+					</article>
+					
+					<a href='#' class='btnLight big'>Voir toutes les études de cas ›</a>
+					<hr>
+
+					<h3 class="light">Wisembly n’est pas seulement efficace pour les événements et séminaires !</h3>
+					<ul id="wisembly-autre">
+						<li>
+							<a href="#">
+								<span class='img'>
+									<img src="layoutImg/tel.png">
+								</span><span class='bloc-txt'>
+									<span class='h3'>Réunions &amp; Conf-calls</span>
+									<span class='p'>Préparer, animer et assurer le suivi de vos réunions &amp; conf&#8209;calls</span>
+									<span class='lien'>découvrir les avantages</span>
+								</span>
+							</a>
+						</li><li>
+							<a href="#">
+								<span class='img'>
+									<img src="layoutImg/pot.png">
+								</span><span class='bloc-txt'>
+									<span class='h3'>Séminaires&nbsp;&amp;&nbsp;Evénements</span>
+									<span class='p'>Donnez la parole à tous les participants de votre séminaire</span>
+									<span class='lien'>découvrir les avantages</span>
+								</span>
+							</a>
+						</li>
+					</ul>
+				</section>
+
+			</div>
+
+			<?php include('includes/footer.php'); ?>
